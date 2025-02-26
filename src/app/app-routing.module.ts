f11b645af9da7ee83416bfa822672622a6165aaa
Bhargav <vyasb1503@gmail.com>
2025-02-26 22:07:14 +0530

    this is the  routhing of the  with the other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ResigterComponent } from './components/resigter/resigter.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path: 'resistor',
+    component: ResigterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/resigter/resigter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resigter',
+  templateUrl: './resigter.component.html',
+  styleUrls: ['./resigter.component.css']
+})
+export class ResigterComponent {
+
+}
